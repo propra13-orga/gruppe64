5e13d92a7c31b0ef2c165af618b4b6f4c6d45ac2
Sebastian Weuste <seweu100@hhu.de>
2013-05-16 21:36:48 +0200

    Player #0001

--- src/com/github/propra13/gruppe64/Player.java
+++ src/com/github/propra13/gruppe64/Player.java
@@ -1,4 +1,4 @@
-package com.github.propra13.gruppe64;
+package com.github.propra13.gruppe64;								// # 0001
 
 public class Player extends Sprite {
 
@@ -8,17 +8,31 @@ public class Player extends Sprite {
 	}
 
 	public void setMot(int i) {
-		// TODO Auto-generated method stub
+		switch (i){
+			case 0: vy= 1; break;
+			case 1: vx= 1; break;
+			case 2: vy=-1; break;
+			case 3: vx=-1; break;
+		}
 		
 	}
-
-	public void unsetMot(int i) {
-		// TODO Auto-generated method stub
-		
+	
+	public void updMot(){
+		int x_off=0;
+		int y_off=0;
+		if(vy==-1) 	y_off=yDim;
+		if(vx==1) 	x_off=xDim;
+		if(was_ist_bei(x+x_off+vx,y+y_off-vy)!='x'){
+			x = x+vx;
+			y = y-vy;
+		}
 	}
 
+	public void unsetMot(int i){
+		switch (i){
+			case 0: vy= 0; break;
+			case 1: vx= 0; break;
+		}
+	}
 	
-
-	
-
 }
