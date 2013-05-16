cd5be4535539ba30792b4563f32b67ed2f138464
Vadim Abramov <vadim.abramov@hhu.de>
2013-05-16 15:30:48 +0200

    Sprite

---
+++ src/com/github/propra13/gruppe64/Sprite.java
@@ -0,0 +1,30 @@
+package com.github.propra13.gruppe64;
+
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
+
+public class Sprite extends JComponent {
+
+	private static final long serialVersionUID = 1419758802002837841L;
+	//Geschwindigkeit bei Bewegung
+	double vx, vy;
+	//Aktuelle Position
+	double x,y;
+	double dimx, dimy;
+	char name;
+	public Sprite(char name) {
+		this.name=name;
+		
+		//switch
+		//unterschiedliche double dimx, dimy;
+		//je nach
+	}
+	public void paintComponent(Graphics g){
+		
+		//switch
+			//g.drawRect
+		//unterschiedliche Sachen malen
+	}
+	
+}
