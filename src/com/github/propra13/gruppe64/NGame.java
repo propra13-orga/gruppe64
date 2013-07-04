2af5feeca0c6dafc5f8847f4f5e7872573c49c11
Vadim Abramov <vadim.abramov@hhu.de>
2013-07-04 23:07:32 +0200

    playerList v2

---
+++ src/com/github/propra13/gruppe64/NGame.java
@@ -0,0 +1,12 @@
+package com.github.propra13.gruppe64;
+
+import java.awt.Container;
+
+public class NGame extends Game {
+
+	public NGame(Container cp, Main main, Controller controller) {
+		super(cp, main, controller);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/github/propra13/gruppe64/NPlayer.java
@@ -0,0 +1,10 @@
+package com.github.propra13.gruppe64;
+
+public class NPlayer extends Player {
+
+	public NPlayer(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+}
