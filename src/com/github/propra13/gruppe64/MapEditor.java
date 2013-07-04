01ac03466b91076ec75cb1b568a21d3982430a8b
Vadim Abramov <vadim.abramov@hhu.de>
2013-07-04 13:16:18 +0200

    ohne Layoutmanger, mit e raum wechseln, branchmerge

---
+++ src/com/github/propra13/gruppe64/MapEditor.java
@@ -0,0 +1,19 @@
+package com.github.propra13.gruppe64;
+
+import java.awt.Container;
+
+public class MapEditor extends Map{
+	Container cp;
+	public MapEditor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MapEditor(Container cp) {
+		super();
+		this.cp=cp;
+	}
+	public void init(){
+		
+	}
+
+}
---
+++ src/com/github/propra13/gruppe64/Switch.java
@@ -0,0 +1,19 @@
+package com.github.propra13.gruppe64;
+
+public class Switch extends Sprite {
+
+	public Switch(int x, int y, int xDim, int yDim) {
+		super(x, y, xDim, yDim);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Switch() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Switch(int xDim, int yDim, char name) {
+		super(xDim, yDim, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
